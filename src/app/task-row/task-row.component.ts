6e03370e9e5fc272fd20a4e0ce105263e9f5f7a9
jlazaro <jlazaro135@gmail.com>
2023-09-02 22:56:21 +0200

    se cambia la fuente de verdad del DOM a typescript

--- src/app/task-row/task-row.component.ts
+++ src/app/task-row/task-row.component.ts
@@ -9,19 +9,14 @@ import { Task, tasks } from './../../assets/tasks'
 export class TaskRowComponent {
   @Input() task!: Task;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
-
-  toggleTasksStatus(e: Event){
-    const taskRow = this.elementRef.nativeElement.querySelector(`#task-${this.task.id}`);
+  toggleTasksStatus(){
     const foundTask: Task | undefined = tasks.find(t => t.id === this.task.id)
     if(foundTask?.isDone){
       foundTask.isDone = false
-      taskRow.classList.remove('bg-green-200', 'line-through');
       return
     } 
     if(foundTask){
       foundTask.isDone = true
-      taskRow.classList.add('bg-green-200', 'line-through');
     } 
   }
 }
